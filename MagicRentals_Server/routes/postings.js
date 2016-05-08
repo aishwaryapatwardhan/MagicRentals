7b7fa06cca0df719ad29c78ff2c7ba1430ba3253
raghavendra <raghavendra1810@gmail.com>
2016-05-08 01:14:22 -0700

    changing login API

--- MagicRentals_Server/routes/postings.js
+++ MagicRentals_Server/routes/postings.js
@@ -6,6 +6,8 @@
 var mongo = require("./dbconfig");
 var mailer = require('./mailer');
 var utils = require('./utils');
+var formidable = require('formidable');
+
 //Add postings
 exports.addPost = function(req, res){
 		
@@ -13,88 +15,121 @@ exports.addPost = function(req, res){
 	
 	var result = {};
 	
-	var user_id = req.param('user_id');
-	var Street = req.param('Street');
-	var City = req.param('City');
-	var State = req.param('State');
-	var Zip = req.param('Zip');
-	var property_type = req.param('property_type');
-	var bath = Number(req.param('bath'));
-	var room = Number(req.param('room'));
-	var area = Number(req.param('area'));
-	var rent = Number(req.param('rent'));
-	var email = req.param('email');
-	var Mobile = req.param('Mobile');
-	var description = req.param('description');
-	var Images = req.param('Images');
-	var other_details = req.param('other_details');
-	var Status = req.param('Status');
-	var view_count = Number(req.param('view_count'));
-	var id;
-	mongo.connect(function(err, db){
+	
+	var form = new formidable.IncomingForm();
+	
+	form.parse(req, function(err, fields, files) {
+	     if(err){
+	       console.log(err);
+	       res.end("sorry, an error occurred");
+	       return;
+	     }
+	    
+//	    console.log(fields.user_id + ": user_id");
+//	    var user_id = fields.user_id;
+//	 	var Street = fields.Street;
+//	 	var City = fields.City;
+//	 	var State = fields.State;
+//	 	var Zip = fields.Zip;
+//	 	var property_type = fields.property_type;
+//	 	var bath = Number(fields.bath);
+//	 	var room = Number(fields.room);
+//	 	var area = Number(fields.area);
+//	 	var rent = Number(fields.rent);
+//	 	var email = req.param(fields.email);
+//	 	var Mobile = req.param(fields.Mobile);
+//	 	var description = req.param(fields.description);
+//	 	var Images = req.param(fields.Images);
+//	 	var other_details = req.param(fields.other_details);
+//	 	var Status = req.param(fields.Status);
+//	 	var view_count = Number(req.param(fields.view_count));
+	 	
+		var user_id = req.param('user_id');
+		var Street = req.param('Street');
+		var City = req.param('City');
+		var State = req.param('State');
+		var Zip = req.param('Zip');
+		var property_type = req.param('property_type');
+		var bath = Number(req.param('bath'));
+		var room = Number(req.param('room'));
+		var area = Number(req.param('area'));
+		var rent = Number(req.param('rent'));
+		var email = req.param('email');
+		var Mobile = req.param('Mobile');
+		var description = req.param('description');
+		var Images = req.param('Images');
+		var other_details = req.param('other_details');
+		var Status = req.param('Status');
+		var view_count = Number(req.param('view_count'));
+	 	
+		var id;
 		
-		if(err){
-			console.log("Unable to connect to mongo");
-			result.code = 209;
-			result.status = "Unable to connect to mongo";
-			res.json(result);
-		}else{
-			console.log("Connected to mongo");
+		mongo.connect(function(err, db){
 			
-			var coll = mongo.collection('rental_posting');
-			
-			id = user_id.replace(/\s+/g, '')+Street.replace(/\s+/g, '')+City.replace(/\s+/g, '')+State+Zip.replace(/\s+/g, '');
-			
-			coll.insertOne( 
-					 {
-						 	"_id" : id,
-							"user_id" : user_id,
-							"address" : {
-									"Street" : Street,
-									"City"   : City,
-									"State"  : State,
-									"Zip"    : Zip
-								    },
-							"property_type" : property_type,
-							"units"  : {
-									"bath" : bath,
-									"room" : room,
-									"area" : area 
-								   },
-							"rent"  : rent,
-							"Contact_info" : {
-									    "email" : email,
-									    "Mobile": Mobile
-									 },
-							"description" : description,
-							"Images" : Images,
-							"other_details" : other_details,
-							"Status" : Status,
-							"view_count" : view_count
-					 },   function(err, docs) {
-					
-						 if(err){
-							 result.code = 208;
-							 result.status = "Duplicate records";
-							 res.json(result);
-						 }else{
-							 utils.notify(id,1,function(){
-								 console.log('notification triggered.');
-								 
-								 mailer.sendMail(function(error, success) {
-									 console.log('Mail sent');
-								 });
-							 })
-							 
-							 result.code = 200; 
-							 result.status = "Successfully inserted";
-							 res.json(result);
-						 }	
+			if(err){
+				console.log("Unable to connect to mongo");
+				result.code = 209;
+				result.status = "Unable to connect to mongo";
+				res.json(result);
+			}else{
+				console.log("Connected to mongo");
+				
+				var coll = mongo.collection('rental_posting');
+				
+				id = user_id.replace(/\s+/g, '')+Street.replace(/\s+/g, '')+City.replace(/\s+/g, '')+State+Zip.replace(/\s+/g, '');
+				
+				coll.insertOne( 
+						 {
+							 	"_id" : id,
+								"user_id" : user_id,
+								"address" : {
+										"Street" : Street,
+										"City"   : City,
+										"State"  : State,
+										"Zip"    : Zip
+									    },
+								"property_type" : property_type,
+								"units"  : {
+										"bath" : bath,
+										"room" : room,
+										"area" : area 
+									   },
+								"rent"  : rent,
+								"Contact_info" : {
+										    "email" : email,
+										    "Mobile": Mobile
+										 },
+								"description" : description,
+								"Images" : Images,
+								"other_details" : other_details,
+								"Status" : Status,
+								"view_count" : view_count
+						 },   function(err, docs) {
 						
-					 }
-			);			
-		}	
-	});	
+							 if(err){
+								 result.code = 208;
+								 result.status = "Duplicate records";
+								 res.json(result);
+							 }else{
+								 utils.notify(id,1,function(){
+									 console.log('notification triggered.');
+									 
+									 mailer.sendMail(function(error, success) {
+										 console.log('Mail sent');
+									 });
+								 })
+								 
+								 result.code = 200; 
+								 result.status = "Successfully inserted";
+								 res.json(result);
+							 }	
+							
+						 }
+				);			
+			}	
+		});	
+	 	
+	});
 };
 
 //getPostings posts
--- MagicRentals_Server/routes/user.js
+++ MagicRentals_Server/routes/user.js
@@ -1,5 +1,6 @@
 /* GET users listing. */
 var mongo = require("./dbconfig");
+var formidable = require('formidable');
 
 exports.list = function(req, res){
   res.send('respond with a resource');
@@ -11,78 +12,88 @@ exports.addUser = function(req, res){
 	
 	var result = {};
 	
-	var uid = req.param('uid');
-	var email = req.param('email');
-	var deviceID = req.param('deviceID');
-	var token = req.param('token');
-	
-	mongo.connect(function(err, db){
-		
-		if(err){
-			console.log("Unable to connect to mongo");
-			result.code = 209;
-			result.status = "Unable to connect to mongo";
-			res.json(result);
-		}else{
-			
-			var coll = mongo.collection('users');
-			coll.findOne(
-					{ "uid" : uid }, function(error, docs){
-						if(error){
-							console.log(error);
-							result.code = 208;
-							result.status = "unable to connect to mongo";
-							res.json(result);
-						}else{
-							if(docs){
-								
-								coll.update(
-										{
-											"uid" : uid
-										}, 
-										{
-											$set: { "email" : email,
-											    	"deviceID" : deviceID,
-												    "token" : token 
-											}
-										}, 
-										function(er, result){
-											if(er){
-												result.code = 208;
-												result.status = "unable to validate user";
-												res.json(result);
-											}else{
-												result.code = 200;
-												result.status = "login successfl";
-												res.json(result);		
-											}
-										});
-																
-							}else{
-								coll.insertOne( 
-										 {
-											 	"uid" : uid,
-												"email" : email,
-												"deviceID" : deviceID,
-												"token" : token
-										 },   function(err, docs) {
-										
-											 if(err){
-												 result.code = 208;
-												 result.status = "Unable login";
-											 }else{
-													result.code = 200;
-													result.status = "sign-up successfl";
-											 }	
-											 res.json(result);
-										 }
-								);
-							}
-						}
-						
-					});
-		}	
-	});
+	form.parse(req, function(err, fields, files) {
+	     if(err){
+	       console.log(err);
+	       res.end("sorry, an error occurred");
+	       return;
+	     }
+	     
+//	    var uid = req.param('uid');
+//	 	var email = req.param('email');
+//	 	var deviceID = req.param('deviceID');
+//	 	var token = req.param('token');
+	    
+		var uid = fields.uid;
+		var email = fields.email;
+		var deviceID = fields.deviceID;
+	    
+	 	mongo.connect(function(err, db){
+	 		
+	 		if(err){
+	 			console.log("Unable to connect to mongo");
+	 			result.code = 209;
+	 			result.status = "Unable to connect to mongo";
+	 			res.json(result);
+	 		}else{
+	 			
+	 			var coll = mongo.collection('users');
+	 			coll.findOne(
+	 					{ "uid" : uid }, function(error, docs){
+	 						if(error){
+	 							console.log(error);
+	 							result.code = 208;
+	 							result.status = "unable to connect to mongo";
+	 							res.json(result);
+	 						}else{
+	 							if(docs){
+	 								
+	 								coll.update(
+	 										{
+	 											"uid" : uid
+	 										}, 
+	 										{
+	 											$set: { "email" : email,
+	 											    	"deviceID" : deviceID
+	 											}
+	 										}, 
+	 										function(er, result){
+	 											if(er){
+	 												result.code = 208;
+	 												result.status = "unable to validate user";
+	 												res.json(result);
+	 											}else{
+	 												result.code = 200;
+	 												result.status = "login successfl";
+	 												res.json(result);		
+	 											}
+	 										});
+	 																
+	 							}else{
+	 								coll.insertOne( 
+	 										 {
+	 											 	"uid" : uid,
+	 												"email" : email,
+	 												"deviceID" : deviceID
+	 										 },   function(err, docs) {
+	 										
+	 											 if(err){
+	 												 result.code = 208;
+	 												 result.status = "Unable login";
+	 											 }else{
+	 													result.code = 200;
+	 													result.status = "sign-up successfl";
+	 											 }	
+	 											 res.json(result);
+	 										 }
+	 								);
+	 							}
+	 						}
+	 						
+	 					});
+	 		}	
+	 	});
+	 });
 };
 
 
