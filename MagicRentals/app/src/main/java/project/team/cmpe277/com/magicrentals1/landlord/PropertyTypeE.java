16ac98559935b94cd27265178eacd31683ff1a3d
Savanigit config --global user.name S <savani.vishnoi@gmail.com>
2016-05-07 19:27:23 -0700

    edit screen, ui changes.. create

--- MagicRentals/app/src/main/java/project/team/cmpe277/com/magicrentals1/landlord/PropertyTypeE.java
+++ MagicRentals/app/src/main/java/project/team/cmpe277/com/magicrentals1/landlord/PropertyTypeE.java
@@ -4,9 +4,20 @@ package project.team.cmpe277.com.magicrentals1.landlord;
  * Created by savani on 5/4/16.
  */
 public enum PropertyTypeE {
-    Apartment,
-    Condo,
-    Villa,
-    Town_House,
-    Penthouse
+    Apartment(0),
+    Condo(1),
+    Villa(2),
+    Town_House(3),
+    Penthouse(4),
+    Cottage(5);
+    private int val;
+    PropertyTypeE(int val){
+        this.val = val;
+    }
+    int getVal(){
+        return val;
+    }
 }
+
+
+
