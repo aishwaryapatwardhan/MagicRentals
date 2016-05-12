2e51cd082d277e06ede2b3a66d633920a20c3c0e
RekhaShankarReddy <rekha.shankar.1409@gmail.com>
2016-05-12 14:47:59 -0700

    Separate Logins

--- MagicRentals/app/src/main/java/project/team/cmpe277/com/magicrentals1/TenantLandlordMainActivity.java
+++ MagicRentals/app/src/main/java/project/team/cmpe277/com/magicrentals1/TenantLandlordMainActivity.java
@@ -1,37 +1,48 @@
 package project.team.cmpe277.com.magicrentals1;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
-public class TenantLandlordMainActivity extends AppCompatActivity implements View.OnClickListener{
+public class TenantLandlordMainActivity extends AppCompatActivity{
+
+
+    private Button TenantBtn;
+    private Button LandlordBtn;
 
     public static final String USER_SELECTED_OPTION = "project.team.cmpe277.com.magicrentals1.TenantLandLordMainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tenant_landlord_main);
+        final TenantLandlordMainActivity tenantLand = this;
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        final Intent i = new Intent(tenantLand, LoginActivity.class);
+        final Context context = getApplicationContext();
+        TenantBtn = (Button) findViewById(R.id.tenantBtn);
+        TenantBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(TenantLandlordMainActivity.this, LoginActivity.class);
+                i.putExtra(USER_SELECTED_OPTION, "t");
+                startActivity(i);
+            }
+        });
+        LandlordBtn = (Button) findViewById(R.id.landlrdBtn);
+        LandlordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(TenantLandlordMainActivity.this, LoginActivity.class);
+                i.putExtra(USER_SELECTED_OPTION, "l");
+                startActivity(i);
+            }
+        });
     }
 
-    @Override
-    public void onClick(View view) {
 
-        int id = view.getId();
-        Intent i = new Intent(this,LoginActivity.class);
-
-        switch (id){
-            case R.id.tenantBtn:
-                i.putExtra(USER_SELECTED_OPTION,"t");
-            case R.id.landlrdBtn:
-                i.putExtra(USER_SELECTED_OPTION,"l");
-        }
-
-        startActivity(i);
-
-    }
 }
