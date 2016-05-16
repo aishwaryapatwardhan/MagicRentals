91b312da1322a8684b3298360022fe8641b6cd42
Savanigit config --global user.name S <savani.vishnoi@gmail.com>
2016-05-16 00:14:23 -0700

    edit screen change and menu chnage

--- MagicRentals/app/src/main/java/project/team/cmpe277/com/magicrentals1/landlord/EditPropertiesActivity.java
+++ MagicRentals/app/src/main/java/project/team/cmpe277/com/magicrentals1/landlord/EditPropertiesActivity.java
@@ -50,7 +50,7 @@ public class EditPropertiesActivity extends AppCompatActivity implements TaskCom
         setContentView(R.layout.fragment_upload_data);
         SharedPreferences sharedPreferences = getSharedPreferences("USER",Context.MODE_PRIVATE);
         userid = sharedPreferences.getString(LoginActivity.USERID,null);
-        int key = getIntent().getExtras().getInt("key");
+        String key = getIntent().getExtras().getString("key");
 
         SharedPreferences preferences = this.getSharedPreferences(TAG, Context.MODE_PRIVATE);
        // userid = preferences.getString(LoginActivity.USERID,null);
@@ -58,6 +58,7 @@ public class EditPropertiesActivity extends AppCompatActivity implements TaskCom
         Log.i(TAG, "Line selected .... "+key);
         mPropertyResultLab = PropertiesResultLab.getPropertiesResultLab(getApplicationContext());
         mPropertyList = mPropertyResultLab.getPropertyList();
+        System.out.println("Size..... "+mPropertyList.size());
         //
 
         for (PropertyModel property : mPropertyList) {
@@ -67,49 +68,45 @@ public class EditPropertiesActivity extends AppCompatActivity implements TaskCom
                 break;
             }
         }
-        System.out.println("Modelll........  +++ "+mPropertyModel.getNickname()+"djdjd  .. "+userid);
-        Spinner spinProprtyType = (Spinner)findViewById(R.id.property_type);
-      //  R.string-spinProprtyType.get
+        if(mPropertyModel != null) {
+            System.out.println("Modelll........  +++ " + mPropertyModel.getNickname() + "djdjd  .. " + userid);
+            Spinner spinProprtyType = (Spinner) findViewById(R.id.property_type);
+            //  R.string-spinProprtyType.get
 //        String tempProperty = mPropertyModel.getProperty_type().toString();
 //            spinProprtyType.setSelection(PropertyTypeE.mPropertyModel.);
 
-     //   spinProprtyType.setSelection(listPropertyTypes.indexOf(mPropertyModel.getProperty_type()));
+            //   spinProprtyType.setSelection(listPropertyTypes.indexOf(mPropertyModel.getProperty_type()));
 
-        area = (EditText)findViewById(R.id.area);
-        area.setText(mPropertyModel.getArea());
-        street = (EditText)findViewById(R.id.street);
-        street.setText(mPropertyModel.getStreet());
-        city = (EditText)findViewById(R.id.city);
-        city.setText(mPropertyModel.getCity());
-        state = ((EditText)findViewById(R.id.state));
-        state.setText(mPropertyModel.getState());
-        zip = (EditText)findViewById(R.id.zipcode);
-        zip.setText(mPropertyModel.getZip());
-        rent = (EditText)findViewById(R.id.rent);
-        rent.setText(mPropertyModel.getRent());
-        email = (EditText)findViewById(R.id.email);
-        email.setText(mPropertyModel.getEmail());
-        mobile = (EditText)findViewById(R.id.phone);
-        mobile.setText(mPropertyModel.getMobile());
-        description = (EditText)findViewById(R.id.description);
-        description.setText(mPropertyModel.getDescription());
-        other_details = (EditText)findViewById(R.id.other_details);
-        other_details.setText(mPropertyModel.getOther_details());
-        nickname = (EditText)findViewById(R.id.nickname);
-        nickname.setText(mPropertyModel.getNickname());
-
-        rooms = (Spinner)findViewById(R.id.roomscount);
-        Log.i(TAG, Integer.parseInt(mPropertyModel.getRoom())+"rooms");
-        rooms.setSelection(Integer.parseInt(mPropertyModel.getRoom())-1);//roomArr.indexOf(mPropertyModel.getRoom()));
-        bath = (Spinner)findViewById(R.id.bathroomcount);
-        rooms.setSelection(Integer.parseInt(mPropertyModel.getBath())-1);
-       // spinProprtyType.setSelection(3);//roomArr.indexOf(mPropertyModel.getBath()));
-        //System.out.println("Hello    ......................."+property);
-
-
-
-
-
+            area = (EditText) findViewById(R.id.area);
+            area.setText(mPropertyModel.getArea());
+            street = (EditText) findViewById(R.id.street);
+            street.setText(mPropertyModel.getStreet());
+            city = (EditText) findViewById(R.id.city);
+            city.setText(mPropertyModel.getCity());
+            state = ((EditText) findViewById(R.id.state));
+            state.setText(mPropertyModel.getState());
+            zip = (EditText) findViewById(R.id.zipcode);
+            zip.setText(mPropertyModel.getZip());
+            rent = (EditText) findViewById(R.id.rent);
+            rent.setText(mPropertyModel.getRent());
+            email = (EditText) findViewById(R.id.email);
+            email.setText(mPropertyModel.getEmail());
+            mobile = (EditText) findViewById(R.id.phone);
+            mobile.setText(mPropertyModel.getMobile());
+            description = (EditText) findViewById(R.id.description);
+            description.setText(mPropertyModel.getDescription());
+            other_details = (EditText) findViewById(R.id.other_details);
+            other_details.setText(mPropertyModel.getOther_details());
+            nickname = (EditText) findViewById(R.id.nickname);
+            nickname.setText(mPropertyModel.getNickname());
+            rooms = (Spinner) findViewById(R.id.roomscount);
+            Log.i(TAG, Integer.parseInt(mPropertyModel.getRoom()) + "rooms");
+            rooms.setSelection(Integer.parseInt(mPropertyModel.getRoom()) - 1);//roomArr.indexOf(mPropertyModel.getRoom()));
+            bath = (Spinner) findViewById(R.id.bathroomcount);
+            rooms.setSelection(Integer.parseInt(mPropertyModel.getBath()) - 1);
+            // spinProprtyType.setSelection(3);//roomArr.indexOf(mPropertyModel.getBath()));
+            //System.out.println("Hello    ......................."+property);
+        }
     }
 
     @Override
